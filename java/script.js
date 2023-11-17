87bc0db64fec6216f10ba157ee42fd86cc68204b
Kevin <kiovit14@gmail.com>
2023-11-17 15:27:21 +0100

    Inserted event listener

--- java/script.js
+++ java/script.js
@@ -7,14 +7,13 @@ function handleMouseDown() {
 function handleMouseOver() {
     if (mousedown) {
       this.style.backgroundColor = "blue";
-      container.appendChild(this.cloneNode(true));
     }
 }
 let mousedown = false;
 
 //Create 16*16 grid of divs
 
-for (let i = 1; i <= 100*100; i++){
+for (let i = 1; i <= 16*16; i++){
     const div = document.createElement("div");
     div.className = "square";
     container.appendChild(div)+i; 
