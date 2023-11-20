70e115aebfa8a5bc96d4d0a79846f18eea02d365
Kevin <kiovit14@gmail.com>
2023-11-20 13:30:06 +0100

    Implemented reset canvas button

--- java/script.js
+++ java/script.js
@@ -1,24 +1,63 @@
-// Define the logic for coloring a square
-function handleMouseDown() {
-    this.style.backgroundColor = "blue";
-  }
+const body = document.querySelector("body");
+let container = document.querySelector(".container");
+let userInput = getUserInput();
+const toggleSwitch = document.getElementById('toggleSwitch');
+// Creates button to clear canvas
+let reset = document.querySelector(".reset");
+reset.addEventListener("click", function() {
+    resetCanvas();
+  });
 
-function handleMouseOver() {
-    if (mousedown) {
-      this.style.backgroundColor = "blue";
-    }
-}
-
-// Mobile functions
-function handleTouchMove() {
-  this.style.backgroundColor = "blue"}
-
+let previousUserInput = userInput;
 let mousedown = false;
 
-// We need to get the number of suqares the user wants
+
+  function resetCanvas() {
+    container.innerHTML = '';
+    mousedown = false; 
+    createGrid(previousUserInput);
+  }
+
+  // Define the logic for coloring a square
+  function handleMouseDown() {
+    if (mousedown && toggleSwitch.checked) {
+      this.style.backgroundColor = getRandomRgb();
+    } else if (mousedown) {
+      this.style.backgroundColor = "blue";
+    }
+  }
+
+  function handleMouseOver() {
+    if (toggleSwitch.checked && mousedown) {
+      this.style.backgroundColor = getRandomRgb();
+    } else if (mousedown) {
+      this.style.backgroundColor = "blue";
+    }
+  }
+
+ // We get the number of suqares the user wants
+ 
+ function createGrid(userPrompt) {
+  for (let i = 1; i <= userPrompt * userPrompt; i++) {
+    container.style.gridTemplateColumns = 'repeat(' + userPrompt + ', 1fr)';
+    const div = document.createElement("div");
+    div.className = "square";
+    container.appendChild(div);
+    div.addEventListener("mousedown", handleMouseDown);
+    div.addEventListener("mouseover", handleMouseOver);
+    document.addEventListener("mouseup", function () {
+    mousedown = false;
+    });
+  }
+
+  document.addEventListener("mousedown", function () {
+    mousedown = true;
+  });
+}
 
 function getUserInput() {
   let userPrompt;
+  
   do {
     userPrompt = prompt("How many squares per side? (20-100)");
     if (userPrompt === null) {
@@ -29,33 +68,18 @@ function getUserInput() {
       alert("Please input a number between 20-100");
     }
   } while (isNaN(userPrompt) || userPrompt < 20 || userPrompt > 100);
+
+  createGrid(userPrompt);
+
   return userPrompt;
-}
+ }
 
-let userInput = getUserInput();
-
-const container = document.querySelector(".container");
-container.style.gridTemplateColumns = 'repeat(' + userInput + ', 1fr)';
-
-//Function that creates the grid
-
-for (let i = 1; i <= userInput*userInput; i++){
-    const div = document.createElement("div");
-    div.className = "square";
-    container.appendChild(div); 
-
-    div.addEventListener("mousedown", handleMouseDown);
-
-    div.addEventListener("mouseover", handleMouseOver);
-    // Mobile event listener
-    div.addEventListener("touchmove", handleTouchMove);
-
-
-    document.addEventListener("mouseup", function() {
-      mousedown = false;
-    });
+ //Function that creates random RGB
+  function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
   }
-
-  document.addEventListener("mousedown", function() {
-    mousedown = true;
-  });
+  
